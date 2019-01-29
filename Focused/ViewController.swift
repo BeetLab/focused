d63db4b09c9d0af5b99420274f431ee4aa74f84b
Никита <nikita.a.semenov@gmail.com>
2019-01-29 16:30:52 +0300

    Initial Commit

---
+++ Focused/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Focused
+//
+//  Created by Krasa on 29/01/2019.
+//  Copyright © 2019 Ronte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
